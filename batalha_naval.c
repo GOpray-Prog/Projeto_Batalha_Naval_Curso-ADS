3a984488d578bb9060529ee4408c4a2f4b0682ea
Guilherme Oliveira <guilherme7oliveira82@gmail.com>
2025-04-01 10:44:18 -0300

    Atualizaçao versao beta1.1

--- batalha_naval.c
+++ batalha_naval.c
@@ -2,8 +2,8 @@
 
 
 int main(){
-   int posicao_linha[3];
-   char posicao_coluna[3];
+   int posicao_linha[6];
+   char posicao_coluna[6];
    int tabuleiro[10][10];
 
 
@@ -22,19 +22,29 @@ int main(){
     printf("   a b c d e f g h i j\n");
 
 
-
+    printf("\nConforme seu tabuleiro, digite a posicao do seu 1º Navio(ex: 2b):\n");
+    printf("Obs: O tamanho do seu navio tem que ser 3, ou seja, ocupar 3 celulas(ex: 2b,3c,4d\n");
     for (int i = 0; i < 3; i++)
     {
-    printf("\nConforme seu tabuleiro, digite a posicao do seu %dº Navio(ex: 2b):\n", i+1);
+    printf("\nTamanho %d\n", i+1);
     scanf(" %d%c", &posicao_linha[i], &posicao_coluna[i]); 
     getchar();
 
     }
 
+    printf("\nConforme seu tabuleiro, digite a posicao do seu 2º Navio(ex: 2b):\n");
+    printf("Obs: O tamanho do seu navio tem que ser 3, ou seja, ocupar 3 celulas(ex: 2b,3c,4d\n");
+    for (int i = 3; i < 6; i++)
+    {
+    printf("\nTamanho %d\n", i-2);
+    scanf(" %d%c", &posicao_linha[i], &posicao_coluna[i]); 
+    getchar();
 
-for (int i = 0; i < 3; i++)
+    }
+
+for (int i = 0; i < 6; i++)
 {  // Verifica os limites do tabuleiro
-if (posicao_coluna[i] - 'a' < 0 || posicao_coluna[i] - 'a' > 9 || posicao_linha[i] < 0 || posicao_linha[i] > 9)
+if (posicao_coluna[i] - 'a' < 0 || posicao_coluna[i] - 'a' > 9 || posicao_linha[i] < 0 || posicao_linha[i] > 9 || posicao_linha[i] == posicao_linha[i])
 {
     printf("Posição inválida!\n");
 } else 
@@ -57,7 +67,7 @@ if (posicao_coluna[i] - 'a' < 0 || posicao_coluna[i] - 'a' > 9 || posicao_linha[
          }
        printf("  %d\n", linha);
       }
-    printf("    a b c d e f g h i j\n");
+    printf("    a b c d e f g h i j\n\n");
    
     
 }
